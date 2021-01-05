93cfe3c10701e097c7175a82ce0c5aa502fdee1b
rhcsky <dev.rhcsky@gmail.com>
2021-01-05 17:30:11 +0900

    Modify main

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from fire import Fire
 
 from utils import *
 from config import get_config
-
 from pprint import pprint as pp
 
 
 def run():
-    # print(Config.test)
-    # print(Config.background_path)
-    # print(Config.evaluation_path)
     pass
 
 
@@ -17,15 +13,5 @@ def download_data():
     download_data()
 
 
-def test():
-    config, config2, config3 = get_config()
-    pp(config.way)
-
-    pp(config2)
-
-    pp(config3)
-
-
 if __name__ == '__main__':
-    # Fire({"run": run, "download-data": download_data})
-    test()
+    Fire({"run": run, "download-data": download_data})
