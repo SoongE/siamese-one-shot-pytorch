7731bc111f83807ca56e5bc88a7cb49f41c9d8f2
rhcsky <dev.rhcsky@gmail.com>
2021-01-05 16:17:06 +0900

    Modify download_data()

--- utils.py
+++ utils.py
@@ -4,23 +4,31 @@ from zipfile import ZipFile
 from glob import glob
 
 
+# download omniglot dataset
 def download_data():
     BASEDIR = os.getcwd() + '/data'
 
-    # download omniglot dataset
     if not os.path.exists(BASEDIR):
         os.mkdir(BASEDIR)
 
-    if not os.path.exists(BASEDIR+'/images_background.zip'):
-        wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_background.zip",BASEDIR)
-    if not os.path.exists(BASEDIR+'/images_evaluation.zip'):
-        wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_evaluation.zip",BASEDIR)
+    if not os.path.exists(BASEDIR + '/images_background.zip'):
+        wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_background.zip",
+                      BASEDIR)
+    if not os.path.exists(BASEDIR + '/images_evaluation.zip'):
+        wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_evaluation.zip",
+                      BASEDIR)
 
-    for d in glob(BASEDIR+'/*.zip'):
+    for d in glob(BASEDIR + '/*.zip'):
         zip_name = os.path.splitext(os.path.basename(d))[0]
         print(f'{zip_name}is being unzipped...', end="")
-        with ZipFile(d,'r') as zip_object:
-            zip_object.extractall(BASEDIR+'/processed/')
+        with ZipFile(d, 'r') as zip_object:
+            zip_object.extractall(BASEDIR + '/processed/')
         print("success")
+
+    try:
+        os.rename(BASEDIR + '/processed/images_background', BASEDIR + '/processed/background')
+        os.rename(BASEDIR + '/processed/images_evaluation', BASEDIR + '/processed/evaluation')
+    except FileNotFoundError as e:
+        print(e)
 
     print("DONE.")
