d3801aadd5b1a451fd1e176fe2582f979233031d
rhcsky <dev.rhcsky@gmail.com>
2021-01-06 11:38:12 +0900

    Modify utils.py

--- utils.py
+++ utils.py
@@ -11,24 +11,30 @@ def download_data():
     if not os.path.exists(BASEDIR):
         os.mkdir(BASEDIR)
 
-    if not os.path.exists(BASEDIR + '/images_background.zip'):
+    if not os.path.exists(BASEDIR + '/raw/images_background.zip'):
+        print("download background image")
         wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_background.zip",
-                      BASEDIR)
-    if not os.path.exists(BASEDIR + '/images_evaluation.zip'):
+                      BASEDIR + '/raw')
+    if not os.path.exists(BASEDIR + '/raw/images_evaluation.zip'):
+        print("download evaluation image")
         wget.download("https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_evaluation.zip",
-                      BASEDIR)
+                      BASEDIR + '/raw')
 
-    for d in glob(BASEDIR + '/*.zip'):
+    for d in glob(BASEDIR + '/raw/*.zip'):
         zip_name = os.path.splitext(os.path.basename(d))[0]
         print(f'{zip_name}is being unzipped...', end="")
         with ZipFile(d, 'r') as zip_object:
-            zip_object.extractall(BASEDIR + '/processed/')
+            zip_object.extractall(BASEDIR + '/unzip/')
         print("success")
 
     try:
-        os.rename(BASEDIR + '/processed/images_background', BASEDIR + '/processed/background')
-        os.rename(BASEDIR + '/processed/images_evaluation', BASEDIR + '/processed/evaluation')
+        os.rename(BASEDIR + '/unzip/images_background', BASEDIR + '/unzip/background')
+        os.rename(BASEDIR + '/unzip/images_evaluation', BASEDIR + '/unzip/evaluation')
     except FileNotFoundError as e:
         print(e)
 
     print("DONE.")
+
+
+if __name__ == '__main__':
+    download_data()
