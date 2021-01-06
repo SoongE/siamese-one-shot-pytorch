f5f313cbef36e01a06a9fe8860b74e87029e5182
rhcsky <dev.rhcsky@gmail.com>
2021-01-06 20:53:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from fire import Fire
 
+from config_make import get_config
+from trainer import Trainer
 from utils import *
-from config import get_config
-from pprint import pprint as pp
+
+
+def train(config=None):
+    if config is None:
+        config = get_config()
+
+    prepare_dirs(config)
+
+    if config.resume:
+        try:
+            save_config(config)
+        except ValueError:
+            print("[!] config data already exist. Either change the model number, or delete the json file and rerun.")
+            return
+
+    trainer = Trainer(config)
+    trainer.train()
+
+
+def test(config=None):
+    if config is None:
+        config = get_config()
 
 
 def run():
-    pass
+    config = get_config()
+    train(config)
+    test()
 
 
 def download_data():
@@ -14,4 +38,5 @@ def download_data():
 
 
 if __name__ == '__main__':
-    Fire({"run": run, "download-data": download_data})
+    # config = get_config()
+    Fire({"run": run, "download-data": download_data, "train": train, })
