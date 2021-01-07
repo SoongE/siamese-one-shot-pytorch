3d49af5b5b844e70251737d754defcb717f467f1
rhcsky <dev.rhcsky@gmail.com>
2021-01-07 13:07:58 +0900

    Add test function

--- main.py
+++ main.py
@@ -5,38 +5,65 @@ from trainer import Trainer
 from utils import *
 
 
-def train(config=None):
-    if config is None:
-        config = get_config()
+def print_status(string):
+    line = '*' * 40
+    print(line + " " + string + " " + line)
 
+
+# only train and validation
+def train(config=None, trainer=None):
+    if config or trainer is None:
+        config = get_config()
+        trainer = Trainer(config)
+
+    # Make directory for save logs and model
     prepare_dirs(config)
 
+    # Check resume data
     if config.resume:
         try:
+            print(f"load saved config data of model number {config.num_model}")
             save_config(config)
         except ValueError:
             print("[!] config data already exist. Either change the model number, or delete the json file and rerun.")
             return
 
-    trainer = Trainer(config)
+    # train model
+    print_status("Train Start")
     trainer.train()
 
 
-def test(config=None):
-    if config is None:
+# only test
+def test(config=None, trainer=None):
+    if config or trainer is None:
         config = get_config()
+        trainer = Trainer(config)
+
+    # test model
+    print_status("Test Start")
+    trainer.test()
 
 
+# running all process. download data, data set, data loader, train, validation, test
 def run():
+    download_omniglot_data()
+
+    # Make options
     config = get_config()
-    train(config)
-    test()
+
+    # Make Trainer
+    trainer = Trainer(config)
+
+    # train
+    train(config, trainer)
+
+    # test
+    test(config, trainer)
 
 
 def download_data():
-    download_data()
+    download_omniglot_data()
 
 
 if __name__ == '__main__':
-    # config = get_config()
-    Fire({"run": run, "download-data": download_data, "train": train, })
+    Fire({"run": run, "download-data": download_data, "train": train, "test": test})
